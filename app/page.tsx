ea1eb7fff06e8f909de3c1221fca6f5dde330f11
Steven Sousa <silver.steven@hotmail.com>
2025-09-25 07:57:21 -0400

    aligned text center for welcoming message

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <>
       <AnimatedSection animation="fade-in-up" duration="duration-700">
         <div className="mt-40 ml-2 mr-2 flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold mb-4">Welcome to the Football App</h1>
-          <p className="text-lg text-gray-700">Your one-stop destination for football stats and updates.</p>
+          <h1 className="text-4xl font-bold mb-4 text-center">Welcome to the Football App</h1>
+          <p className="text-lg text-gray-700 text-center">Your one-stop destination for football stats and updates.</p>
         </div>
         <Standings tournament="bra.1" season="2025" />
       </AnimatedSection>
