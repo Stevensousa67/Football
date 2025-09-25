22157e02043e78cdf259f98b3bad6109af8876d2
Steven Sousa <silver.steven@hotmail.com>
2025-09-25 07:57:32 -0400

    created news api endpoint

---
+++ app/api/espn/news.ts
@@ -0,0 +1,12 @@
+export async function getStandings(tournament: string) {
+  const res = await fetch(
+    `https://site.web.api.espn.com/apis/v2/sports/soccer/${tournament}/news`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data", { cause: res.status });
+  }
+
+  const data = await res.json();
+  return data;
+}
