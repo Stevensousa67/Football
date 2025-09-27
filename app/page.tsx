18afcdc10f2fae6dc0ec29483fe969f94e1d579a
Steven Sousa <silver.steven@hotmail.com>
2025-09-27 13:23:32 -0400

    added news section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AnimatedSection from "@/components/AnimatedSection";
 import Contact from "@/components/sections/contact/Contact";
+import News from "@/components/sections/news/News";
 import Standings from "@/components/sections/standings/Standings";
 
 export default function Home() {
@@ -11,6 +12,9 @@ export default function Home() {
           <p className="text-lg text-gray-700 text-center">Your one-stop destination for football stats and updates.</p>
         </div>
         <Standings tournament="bra.1" season="2025" />
+      </AnimatedSection>
+      <AnimatedSection animation="fade-in-up" duration="duration-700">
+        <News tournament="bra.1" />
       </AnimatedSection>
       <AnimatedSection animation="fade-in-up" duration="duration-700">
         <Contact />
