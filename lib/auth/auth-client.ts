1e5d1382787857355918d0545978c12ef0df78f8
Steven Sousa <silver.steven@hotmail.com>
2026-01-27 22:01:53 -0500

    fix: remove fallback baseURL in auth configuration to ensure BETTER_AUTH_URL is used

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"; // make sure to import fro
 import { APIError } from "better-auth/api";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
 });
 
 export const loginWithSocial = async (provider: string) => {
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -6,7 +6,7 @@ import { PrismaClient } from "@/generated/prisma";
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
